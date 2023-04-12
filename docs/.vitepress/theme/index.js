d2e3f57ba67c82ba9fa1b03bd2dc8c1d382efd77
吴笠华 <390405712@qq.com>
2023-04-12 09:59:10 +0800

    v1.2.2

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,20 +4,30 @@
 import DefaultTheme from "vitepress/theme"
 import "element-plus/dist/index.css";
 import elementplus from "element-plus"
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 // import * as elementPlusGenerator from "element-plus-generator"
 // import { FormGenerator, TableGenerator } from "element-plus-generator"
 import './style/var.css'
 
+if (typeof window === 'undefined') {
+  global.document = {
+    createElement: () => { },
+    createTextNode: () => { },
+    head: {
+      appendChild: () => { }
+    },
+    querySelectorAll: () => []
+  }
+}
 export default {
   ...DefaultTheme,
   enhanceApp: async ({ app, router, siteData }) => {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
     app.use(elementplus, {
-      locale: zhCn,
+      // locale: zhCn,
     });
     // app.component('FormGenerator', FormGenerator)
     // app.component('TableGenerator', TableGenerator)
